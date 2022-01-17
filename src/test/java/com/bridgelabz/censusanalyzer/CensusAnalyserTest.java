fec555508721e0ffd07c318dbc6c80ca1eecc7f6
Ravi3599 <ravikantdukre2018@gmail.com>
2022-01-17 17:57:51 +0530

    ADDED: test case to ensure number of records matches

---
+++ src/test/java/com/bridgelabz/censusanalyzer/CensusAnalyserTest.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.censusanalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CensusAnalyserTest {
+
+	private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndianStatesCensus.csv";
+	
+	
+	@Test
+	public void givenIndianCensusCSVFile_WhenCorrectPath_ShouldReturnCorrectRecords() {
+		try {
+			CensusAnalyser censusAnalyser = new CensusAnalyser();
+			int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+			Assert.assertEquals(33, numOfRecords);
+		} 
+		catch (CensusAnalyserException e) {
+		}
+	}
+
+}
